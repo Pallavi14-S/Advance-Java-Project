ab56bc68e43d63bab781d878f01def2f82b9bdcd
Kaustubh Gade <123086264+Kaustubh5591@users.noreply.github.com>
2025-06-07 09:22:12 +0530

    Update StudentService.java

--- spring_boot_backend_template/src/main/java/com/cdac/service/StudentService.java
+++ spring_boot_backend_template/src/main/java/com/cdac/service/StudentService.java
@@ -3,6 +3,7 @@ package com.cdac.service;
 import java.util.List;
 
 import com.cdac.dto.CourseRespDTO;
+import com.cdac.dto.StudentLoginRequestDTO;
 import com.cdac.dto.StudentRequestDTO;
 import com.cdac.dto.StudentResponseDTO;
 
@@ -13,5 +14,6 @@ public interface StudentService {
     List<CourseRespDTO> getEnrolledCourses(Long studentId);
     StudentResponseDTO enrollInCourse(Long studentId, Long courseId);
     StudentResponseDTO unenrollFromCourse(Long studentId, Long courseId);
- List<StudentResponseDTO> getEnrollStudent( Long courseId);
+    List<StudentResponseDTO> getEnrollStudent(Long courseId);
+    StudentResponseDTO loginStudent(StudentLoginRequestDTO loginDTO);
 } 
